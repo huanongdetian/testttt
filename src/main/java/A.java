482f8fa2ace7a8720cf68fb39c0607bb58e9c2aa
huanongdetian <2477606418@qq.com>
2018-08-01 10:08:47 +0800

    devfenzhi

---
+++ src/main/java/A.java
@@ -0,0 +1,11 @@
+/**
+ * @description：
+ * @author: PanNaiZhao
+ * @date: 2018-08-01 上午 10:08
+ * @version: 1.0.0
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
